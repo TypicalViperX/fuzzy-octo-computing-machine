17b2122f7acab2fcf9d167ccdf215abff46948c6
TypicalViperX <81366100+TypicalViperX@users.noreply.github.com>
2022-10-06 01:12:33 +0200

    Create conf.py

---
+++ conf.py
@@ -0,0 +1,8 @@
+from apdbot.sample_con import Config
+
+
+class Config(Config): 
+   BOT_TOKEN = "5727317456:AAGx-tReqdMg6RiZB6GL75m_yn0-wE8OmqY" 
+   APP_ID = 1773284 
+   API_HASH = "d2fff472e04558675e3a09d1f09b1d77" 
+   AUTH = [-1001875207293]
